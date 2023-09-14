f149844829a3a510cdba740ff327d614130f6c09
Charles Bell <chbell@frizzle08.device.utk.edu>
2023-09-14 15:42:23 -0400

    edited py

--- converter.py
+++ converter.py
@@ -12,9 +12,17 @@ if len(sys.argv) < 3:
 
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
-f = ROOT.TFile.Open(inputFile, "READ")    
+#f = ROOT.TFile.Open(inputFile, "READ")    
+f = ROOT.TGeoManager.Import(inputFile)
 g = ROOT.TFile.Open(outputFile, "RECREATE")
 
-geo = f.default
+ROOT.gGeoManager.DefaultColors()
+ROOT.gGeoManager.SetPhiRange(phimin=0., phimax=270.)
 
-g.WriteObject(geo, "New_default")
+#print(type(f))
+new_phi = f
+#new_phi.Edit()
+#new_phi.SetPhiRange(phimin=0., phimax=270.)
+#print(type(new_phi))
+#new_phi.CloseGeometry()
+g.WriteObject(new_phi, "default")
